54204631b1cfdd7a8387f0f663ef8538a48e7ccb
Lisa Patel <lisapatel@prwd.co.uk>
2018-05-06 13:50:56 +0100

    debugging errors with click evts

--- tabs.js
+++ tabs.js
@@ -1,19 +1,30 @@
-fireTabs() => {
+const fireTabs = function() {
 	const tabTrigger = document.querySelectorAll('.js-trigger');
 	const tabParent = document.querySelectorAll('.js-tab');
-
+	console.log(tabTrigger, tabParent);
 	for(tt in tabTrigger) {
+		let el = tabTrigger[tt];
 		let tabEl = tabTrigger[tt].parentElement;
-		tabTrigger[tt].addEventListener('click', (tabEl) => {
-			this.onClick(tabEl)
+
+		//console.log(el, tabEl);
+
+		el.addEventListener('click', (tabEl) => {
+			el.onClick(tabEl);
 		});
 	}
+}
 
-	onClick(tabEl) => {
-		if (tabEl.classList.contains('isActive')) {
-			
+const removeActive = (tabParent) => {
+	for(tp in tabParent) {
+		if (tabParent[tp].classList.contains('isActive')) {
+			tabParent[tp].classList.remove('isActive');
 		}
 	}
+}
+
+const onClick = (tabEl) => {
+	removeActive(tabParent);
+	tabEl.classList.add('isActive');
 }
 
 
