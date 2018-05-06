6ba7d0599bed88d95262511ff7a846ea7d250f8e
Lisa Patel <lisapatel@prwd.co.uk>
2018-05-06 13:15:06 +0100

    figuring out the functionality

--- tabs.js
+++ tabs.js
@@ -1,1 +1,20 @@
-console.log('log from tabs.js');
+fireTabs() => {
+	const tabTrigger = document.querySelectorAll('.js-trigger');
+	const tabParent = document.querySelectorAll('.js-tab');
+
+	for(tt in tabTrigger) {
+		let tabEl = tabTrigger[tt].parentElement;
+		tabTrigger[tt].addEventListener('click', (tabEl) => {
+			this.onClick(tabEl)
+		});
+	}
+
+	onClick(tabEl) => {
+		if (tabEl.classList.contains('isActive')) {
+			
+		}
+	}
+}
+
+
+window.onload = fireTabs();
