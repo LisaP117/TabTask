5d6a5b0d9d55d2430c31cd910bd545a2075b93fa
Lisa Patel <lisapatel@prwd.co.uk>
2018-05-06 14:54:15 +0100

    checking this scope in different functions

--- tabs.js
+++ tabs.js
@@ -4,14 +4,15 @@ class tabs {
 		this.triggerParent = document.querySelectorAll(triggerParent);
 	}
 
-	fireTabs(trigger, triggerParent) {
+	fireTabs() {
 		for(let tt of this.trigger) {
 			tt.addEventListener('click', this.onClick, false);
 		}
+		console.log(this.triggerParent);
 	}
 
-	removeActive() {
-		for(let tp of this.triggerParent) {
+	removeActive(triggerParent) {
+		for(let tp of triggerParent) {
 			if (tp.classList.contains('isActive')) {
 				tp.classList.remove('isActive');
 			}
