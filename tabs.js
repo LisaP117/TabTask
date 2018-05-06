3a23e9bd94d6f9a8c488bd6207eadd026305fed7
Lisa Patel <lisapatel@prwd.co.uk>
2018-05-06 15:06:16 +0100

    not got the use of class correct, removing the other active states function isn't correct, will investigate in my own time as i have been on this over an hour

--- tabs.js
+++ tabs.js
@@ -1,29 +1,30 @@
 class tabs {
-	constructor(trigger, triggerParent) {
+	constructor(trigger/*, triggerParent*/) {
 		this.trigger = document.querySelectorAll(trigger);
-		this.triggerParent = document.querySelectorAll(triggerParent);
+		//this.triggerParent = document.querySelectorAll(triggerParent);
 	}
 
 	fireTabs() {
 		for(let tt of this.trigger) {
 			tt.addEventListener('click', this.onClick, false);
 		}
-		console.log(this.triggerParent);
 	}
 
-	removeActive(triggerParent) {
-		for(let tp of triggerParent) {
+	onClick() {
+		const tabEl = this.parentNode;
+		const sibEls = tabEl.siblings;
+		this.removeActive(sibEls);
+		tabEl.classList.add('isActive');
+	}
+
+	removeActive(sibEls) {
+		for(let tp of sibEls) {
 			if (tp.classList.contains('isActive')) {
 				tp.classList.remove('isActive');
 			}
 		}
 	}
-
-	onClick() {
-		const tabEl = this.parentNode;
-		tabEl.classList.add('isActive');
-	}
 }
 
-let ft = new tabs('.js-trigger','.js-tab');
+let ft = new tabs('.js-trigger'/*,'.js-tab'*/);
 ft.fireTabs();
